7885aa033528da20489e1b2061e3fc9f3de91ff0
Damien Jiang <jiangtyd@gmail.com>
2017-09-19 03:01:28 -0700

    filtering out current player in merge actually works

--- webapp/app/players/controllers/playerTypeahead.controller.js
+++ webapp/app/players/controllers/playerTypeahead.controller.js
@@ -1,5 +1,11 @@
 angular.module('app.players')
   .controller("PlayerTypeaheadController", function($scope, PlayerService) {
     $scope.playerService = PlayerService;
+
+    $scope.notEqual = function(id) {
+      return function(player) {
+        return player.id != id;
+      }
+    };
   });
 
