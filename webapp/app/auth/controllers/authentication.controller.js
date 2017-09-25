571208d0aac2f1d429bb6a5c114b051b1a6deff8
Damien Jiang <jiangtyd@gmail.com>
2017-09-25 04:28:21 -0700

    rm some dependencies in AuthenticationController

--- webapp/app/auth/controllers/authentication.controller.js
+++ webapp/app/auth/controllers/authentication.controller.js
@@ -1,4 +1,4 @@
-angular.module('app.auth').controller("AuthenticationController", function($scope, $uibModal, AuthenticationService, SessionService, RegionService) {
+angular.module('app.auth').controller("AuthenticationController", function($scope, $uibModal, AuthenticationService) {
     $scope.postParams = {};
     $scope.errorTxt = "";
 
